73318416169c1ef3c4176bf0f72a26690a884476
dmh <dmh@unidata.ucar.edu>
2015-01-30 19:56:24 -0700

    rebase

--- cf.cmake
+++ cf.cmake
@@ -1,16 +1,20 @@
-rm -fr build
-mkdir build
-cd build
-PREFIX=/usr/local
-CPPATH="/machine/dmh/libs;${CMAKE_PREFIX_PATH}"
+#!/bin/sh
+
+# These are installation dependent
+PREFIX=t:/cygwin/usr/local
+CPPATH="${PREFIX};t:/cygwin/usr/local/cmake/HDF5"
 #NONC4="-DENABLE_NETCDF_4=OFF"
-#HDF5="-DHDF5_LIB=${PREFIX}/lib/libhdf5.so -DHDF5_HL_LIB=${PREFIX}/lib/libhdf5_hl.so -DHDF5_INCLUDE_DIR=${PREFIX}/include"
 
 FLAGS="-DCMAKE_PREFIX_PATH=$CPPATH"
 FLAGS="$FLAGS -DCMAKE_INSTALL_PREFIX=${PREFIX}"
 FLAGS="$FLAGS -DENABLE_DAP_REMOTE_TESTS=true"
 FLAGS="$FLAGS -DENABLE_DAP_AUTH_TESTS=true"
-#FLAGS="$FLAGS $HDF5"
+
+rm -fr build
+mkdir build
+cd build
+
 cmake $FLAGS ..
-cmake --build .
-#make test
+#cmake --build .
+#cmake --build . --target test
+#cmake --build . --target install 
