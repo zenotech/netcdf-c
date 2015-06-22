cb8fe432492ef5c97a4087b1c8aace0445fb02b8
Ward Fisher <wfisher@unidata.ucar.edu>
2015-06-22 13:26:10 -0600

    Corrected an API issue, doh.

--- ncdump/unicode_ncdump.c
+++ ncdump/unicode_ncdump.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
 
-#ifdef _MSC_VER 
+#ifdef _MSC_VER
 #include <io.h>
 #endif
 
 
 int main(int argc, char *argv[]) {
 
+  int i = 0;
+
   printf("Running Unicode Command-Line Test.\n");
   printf("----------------------------------\n");
   printf("* argc:\t%d\n",argc);
-  for(int i = 0; i < argc; i++) {
+  printf("Arguments:\n");
+  for(i = 0; i < argc; i++) {
     printf("* argv[%d]: %s\n",i,argv[i]);
   }
 
