a830b723401b4917561587657616eba83572b416
Ward Fisher <wfisher@ucar.edu>
2015-06-22 13:45:23 -0600

    Additional changes to test program.

--- ncdump/unicode_ncdump.c
+++ ncdump/unicode_ncdump.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 
 #ifdef _MSC_VER
+#include <windows.h>
+#include <tchar.h>
 #include <io.h>
 #endif
 
-
+#ifdef _MSC_VER
+int wmain(int argc, wchar_t *argv[], TCHAR *envp[]) {
+#else
 int main(int argc, char *argv[]) {
 
   int i = 0;
