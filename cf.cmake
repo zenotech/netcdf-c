9753acbd9dc1cb1c78cdc6ef364611ed34c3b1fb
dmh <dmh@unidata.ucar.edu>
2015-11-28 21:13:51 -0700

    cf.cmake

--- cf.cmake
+++ cf.cmake
@@ -1,25 +1,41 @@
 rm -fr build
 mkdir build
 cd build
+
+#PARALLEL=1
+
 UL=/usr/local
+
+LB=lib
+
 PPATH="$UL"
-if test -f ${UL}/lib/libz.so ; then
+if test -f ${UL}/$LB/libz.so ; then
 ZL=${UL}/lib
-elif test -f /usr/lib/libz.so ; then
+elif test -f /usr/$LB/libz.so ; then
 ZL=/usr/lib
-elif test -f /lib/libz.so ; then
+elif test -f /LB/libz.so ; then
 ZL=/lib
+fi
+
+if test "x$PARALLEL" = x1 ; then
+  MPI=/usr/local
+  PATH=${PATH}:$MPI/bin
+  CC="$MPI/bin/mpicc"
+  CPPFLAGS="-I${MPI}/include -I${MPI}/include -I${MPI}/include"
+  LDFLAGS="-L${MPI}/lib -L${MPI}/lib -L${MPI}/lib"
+  LDLIBS="-lmpich"
 fi
 
 ZLIB="-DZLIB_LIBRARY=${ZL}/libz.so -DZLIB_INCLUDE_DIR=${UL}/include -DZLIB_INCLUDE_DIRS=${UL}/include"
 
-HDF5="-DHDF5_LIB=${UL}/lib/libhdf5.so -DHDF5_HL_LIB=${UL}/lib/libhdf5_hl.so -DHDF5_INCLUDE_DIR=${UL}/include"
-CURL="-DCURL_LIBRARY=${UL}/lib/libcurl.so -DCURL_INCLUDE_DIR=${UL}/include -DCURL_INCLUDE_DIRS=${UL}/include"
+HDF5="-DHDF5_LIB=${UL}/$LB/libhdf5.so -DHDF5_HL_LIB=${UL}/$LB/libhdf5_hl.so -DHDF5_INCLUDE_DIR=${UL}/include"
+CURL="-DCURL_LIBRARY=${UL}/$LB/libcurl.so -DCURL_INCLUDE_DIR=${UL}/include -DCURL_INCLUDE_DIRS=${UL}/include"
 FLAGS="-DCMAKE_PREFIX_PATH=$PPATH"
 FLAGS="$FLAGS -DCMAKE_INSTALL_PREFIX=${UL}"
 FLAGS="$FLAGS -DCMAKE_PREFIX_PATH=$PPATH"
 FLAGS="$FLAGS -DENABLE_DAP_REMOTE_TESTS=true"
 FLAGS="$FLAGS -DENABLE_DAP_AUTH_TESTS=true"
+
 cmake $FLAGS ${ZLIB} ${HDF5} ${CURL} ..
-cmake --build .
+#cmake --build .
 #cmake --build . --target test
