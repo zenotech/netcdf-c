d943b28f548adefaf59476da17d7c8638bdc4b27
dmh <dmh@unidata.ucar.edu>
2015-11-28 19:53:49 -0700

    ckp

--- cf.cmake
+++ cf.cmake
@@ -3,7 +3,16 @@ mkdir build
 cd build
 UL=/usr/local
 PPATH="$UL"
-ZLIB="-DZLIB_LIBRARY=${UL}/lib/libz.so  -DZLIB_INCLUDE_DIR=${UL}/include"
+if test -f ${UL}/lib/libz.so ; then
+ZL=${UL}/lib
+elif test -f /usr/lib/libz.so ; then
+ZL=/usr/lib
+elif test -f /lib/libz.so ; then
+ZL=/lib
+fi
+
+ZLIB="-DZLIB_LIBRARY=${ZL}/libz.so -DZLIB_INCLUDE_DIR=${UL}/include -DZLIB_INCLUDE_DIRS=${UL}/include"
+
 HDF5="-DHDF5_LIB=${UL}/lib/libhdf5.so -DHDF5_HL_LIB=${UL}/lib/libhdf5_hl.so -DHDF5_INCLUDE_DIR=${UL}/include"
 CURL="-DCURL_LIBRARY=${UL}/lib/libcurl.so -DCURL_INCLUDE_DIR=${UL}/include -DCURL_INCLUDE_DIRS=${UL}/include"
 FLAGS="-DCMAKE_PREFIX_PATH=$PPATH"
@@ -12,5 +21,5 @@ FLAGS="$FLAGS -DCMAKE_PREFIX_PATH=$PPATH"
 FLAGS="$FLAGS -DENABLE_DAP_REMOTE_TESTS=true"
 FLAGS="$FLAGS -DENABLE_DAP_AUTH_TESTS=true"
 cmake $FLAGS ${ZLIB} ${HDF5} ${CURL} ..
-#cmake --build .
+cmake --build .
 #cmake --build . --target test
