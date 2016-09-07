4a56be5ed8db4570ed7e6791a6f36d0a2a21c682
Ward Fisher <wfisher@ucar.edu>
2016-09-07 14:12:58 -0600

    Updated bootstrap script locally.

--- vagrant-scripts/bootstrap.sh
+++ vagrant-scripts/bootstrap.sh
@@ -5,4 +5,7 @@ set -u
 
 apt-get update
 apt-get upgrade
-apt-get -y install emacs ubuntu-dev-tools mpich libmpich-dev libcurl4-openssl
+apt-get -y install emacs ubuntu-dev-tools mpich libmpich-dev libcurl4-openssl-dev  
+autoconf libtool libmxml-dev libtool
+cp /vagrant/vagrant-scripts/*.sh /home/vagrant/Desktop
+chown -R vagrant:vagrant /home/vagrant
