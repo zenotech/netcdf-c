3ef849bd9135ef8a9320652e8e85fdff65d22235
Ward Fisher <wfisher@ucar.edu>
2016-09-07 16:26:19 -0600

    More bootstrap updates.

--- vagrant-scripts/bootstrap.sh
+++ vagrant-scripts/bootstrap.sh
@@ -2,10 +2,11 @@
 
 set -u
 
-
 apt-get update
 apt-get upgrade -y
 
 apt-get -y install emacs ubuntu-dev-tools mpich libmpich-dev libcurl4-openssl-dev autoconf libtool libmxml-dev libtool
-cp /vagrant/vagrant-scripts/*.sh /home/vagrant/Desktop
+cp /vagrant/vagrant-scripts/install_hdf5tune*.sh /home/vagrant/Desktop
 chown -R vagrant:vagrant /home/vagrant
+sudo -u vagrant /home/vagrant/Desktop/install_hdf5tune_deps.sh
+sudo -u vagrant /home/vagrant/Desktop/install_hdf5tune.sh
