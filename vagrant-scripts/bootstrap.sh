51bc6debc6fde0c03cd64902afbfa8270dc8cf90
Ward Fisher <wfisher@ucar.edu>
2016-09-07 10:33:17 -0600

    Updated bootstrap script.

--- vagrant-scripts/bootstrap.sh
+++ vagrant-scripts/bootstrap.sh
@@ -5,4 +5,4 @@ set -u
 
 apt-get update
 apt-get upgrade
-apt-get -y install emacs ubuntu-dev-tools mpich libmpich-dev libcurl4-openssl
+apt-get -y install emacs ubuntu-dev-tools mpich libmpich-dev libcurl4-openssl libtool autoconf
