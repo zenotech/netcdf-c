30189c33d786a5c94ab843f2a4274e390779f42e
Ward Fisher <wfisher@ucar.edu>
2016-09-08 15:39:28 -0600

    Further refinements of bootstrap script.

--- vagrant-scripts/bootstrap.sh
+++ vagrant-scripts/bootstrap.sh
@@ -7,6 +7,10 @@ apt-get upgrade -y
 
 apt-get -y install emacs ubuntu-dev-tools mpich libmpich-dev libcurl4-openssl-dev autoconf libtool libmxml-dev libtool
 cp /vagrant/vagrant-scripts/install_hdf5tune*.sh /home/vagrant/Desktop
-chown -R vagrant:vagrant /home/vagrant
+
 sudo -u vagrant /home/vagrant/Desktop/install_hdf5tune_deps.sh
 sudo -u vagrant /home/vagrant/Desktop/install_hdf5tune.sh
+
+git clone http://github.com/Unidata/netcdf-c /home/vagrant/Desktop/netcdf-c
+
+chown -R vagrant:vagrant /home/vagrant
