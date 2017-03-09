3dac2be9450e8e28a191074fbf0c58d1c6289d57
Ward Fisher <wfisher@ucar.edu>
2017-03-09 12:17:58 -0700

    Bumped hdf5 version in utility script.

--- vagrant-scripts/install_hdf5tune_deps.sh
+++ vagrant-scripts/install_hdf5tune_deps.sh
@@ -19,9 +19,9 @@ popd
 
 
 echo "Installing hdf5"
-wget http://www.hdfgroup.org/ftp/HDF5/releases/hdf5-1.8.17/src/hdf5-1.8.17.tar.gz
-tar xvzf hdf5-1.8.17.tar.gz
-pushd hdf5-1.8.17
+wget https://support.hdfgroup.org/ftp/HDF5/current18/src/hdf5-1.8.18.tar.gz
+tar xvzf hdf5-1.8.18.tar.gz
+pushd hdf5-1.8.18
 CC=mpicc FC=mpif90 CXX=mpic++ ./configure --prefix=/usr --enable-parallel --with-zlib=/usr --enable-hl --enable-shared --disable-static --enable-using-memchecker --enable-debug=all --enable-codestack
 make -j 4 && sudo make install
 popd
