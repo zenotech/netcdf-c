2b448f40728d70962734c9ad1067066048166740
Dennis Heimbigner <dmh@ucar.edu>
2018-01-14 21:21:29 -0700

    take18

--- nc_test4/test_wrapper.c
+++ nc_test4/test_wrapper.c
@@ -1,11 +1,24 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+/*
+This is a round-about way of getting the output from
+a shell script test. Apparently, LOG_DRIVER
+only works for executables and not for shell scripts.
+So, use this executable to indirectly invoke a shell script
+test so that its output appears in e.g. travis log.
+If I could figure out the shell equivalent of LOG_DRIVER,
+then this would not be necessary.
+*/
+
 
 #define CMD "./tst_filter.sh"
 
 int
 main(int argc, char** argv)
 {
-    system(CMD);
-    return 0;
+    int stat = system(CMD);
+    return WEXITSTATUS(stat);
 }
